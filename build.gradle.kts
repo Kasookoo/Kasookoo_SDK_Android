ae94e74235fb13cc724b9c38fccb702d4fad4d89
Ali Haider <88846897+alihaidergrw-github@users.noreply.github.com>
2025-10-17 19:05:13 +0500

    build gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish") // ADD THIS
 }
 
 android {
@@ -9,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 26
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -30,47 +30,19 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
-dependencies {
-    implementation ("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation ("androidx.activity:activity-ktx:1.7.2")
+afterEvaluate {
+    publishing {
+        publications {
+            release(MavenPublication) {
+                groupId = 'com.github.Kasookoo' // Replace with your GitHub username
+                artifactId = 'Kasookoo_SDK_Android'                         // SDK name
+                version = '1.0.0'                            // Version
 
-//    implementation("androidx.core:core-ktx:1.17.0")
-//    implementation("androidx.appcompat:appcompat:1.7.1")
-//    implementation("com.google.android.material:material:1.13.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-//    implementation ("com.google.android.material:material:1.9.0")
-//    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-//    implementation ("androidx.activity:activity-ktx:1.7.2")
-
-    // LiveKit SDK - Using stable version
-    implementation ("io.livekit:livekit-android:2.20.2")
-//    implementation ("io.livekit:livekit-android:1.5.0'
-
-    // Firebase for push notifications
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
-    implementation ("com.google.firebase:firebase-messaging-ktx")
-
-    // Networking
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
-    // JSON parsing for call history
-    implementation ("com.google.code.gson:gson:2.10.1")
-
-    // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-
+                // This points to your AAR file
+                artifact("$projectDir/libs/mysdk-release.aar")
+            }
+        }
+    }
 }
