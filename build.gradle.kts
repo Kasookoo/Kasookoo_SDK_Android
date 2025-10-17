c3fd7b85666ac5cfd121b2c9c466f3837093e5dc
Ali Haider <88846897+alihaidergrw-github@users.noreply.github.com>
2025-10-17 19:20:46 +0500

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ afterEvaluate {
                 version = '1.0'                            // Version
 
                 // This points to your AAR file
-                artifact("$projectDir/libs/mysdk-release.aar")
+                artifact("$projectDir/libs/Kasookoo_SDK_Android.aar")
             }
         }
     }
